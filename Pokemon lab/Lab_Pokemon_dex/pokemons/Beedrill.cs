614a0aff4849d70243da162bbbbc238570574abc
ThanapronChanidkul <119285762+ThanapronChanidkul@users.noreply.github.com>
2023-02-12 19:39:25 +0700

    Add files via upload

---
+++ Pokemon lab/Lab_Pokemon_dex/pokemons/Beedrill.cs
@@ -0,0 +1,23 @@
+﻿using Lab_Pokemon_dex.pokemons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Pokemon_dex
+{
+    internal class Beedrill : Pokemon
+    {
+        public Beedrill() 
+        {
+            Random random = new Random();
+            this.name = "Bulbasaur";
+            //random number between >= 430 and < 525
+            this.HP = random.Next(500, 1050);
+            this._image = Properties.Resources._001;
+            this.Attack = random.Next(166, 307);
+            this.Defense = random.Next(10,50);
+        }
+    }
+}
